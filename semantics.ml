f00abdc30a08f9f3dc0731d3b4abdc12daa58529
Pierre Donat-Bouillud <pierre.donat-bouillud@ircam.fr>
2019-11-07 11:56:15 +0100

    Definition of buffers

--- semantics.ml
+++ semantics.ml
@@ -2,7 +2,19 @@
 open Batteries
 
 (** Buffers *)
-type samplebuffer = float array
-type latency = float
-type period = float
-type buffer = latency * period * samplebuffer
+module Buffer = struct
+  (* Type definitions (our domains) *)
+  type samplebuffer = float array
+  type latency = float
+  type period = float
+  type buffer = latency * period * samplebuffer
+
+  let latency (lat, _, _)  = lat
+  let period (_, p, _) = p
+  let samplebuffer (_,_, spbuf) = spbuf
+
+  let length buffer = Array.length (samplebuffer buffer)
+  let (@+) (lat, p, spbuf) latency = (lat + latency, p, spbuf)
+end 
+
+(** *)
